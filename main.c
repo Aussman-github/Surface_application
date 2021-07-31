2a1d8b88d1291c378e2ebddf52f8f1f1f6c1590e
aussman <aussman.id@gmail.com>
2021-07-31 21:32:18 +0200

    add additional zones

--- main.c
+++ main.c
@@ -5,7 +5,10 @@
 #define LEFT_ZONE (2)
 #define BACK_ZONE (3)
 #define RIGHT_ZONE (4)
-
+#define FRONT_LEFT (5)
+#define FRONT_RIGHT (6)
+#define BACK_LEFT (7)
+#define BACK_RIGHT (8)
 
 
 /* static variables */
