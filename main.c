1e69ddf49d86cc5b36d3030211f35ef0ce6c2c94
aussman <aussman.id@gmail.com>
2021-07-31 21:28:21 +0200

    Add function sendBleMsg

--- main.c
+++ main.c
@@ -6,19 +6,18 @@
 #define BACK_ZONE (3)
 #define RIGHT_ZONE (4)
 
+#ifndef
+#define ENABLE_BLE (1)
+#endif
 
+void sendBleMsg(void);
 
 /* static variables */
 static uint32_t counter;
 static uint32_t counter2;
 
 
-int main(int argc, char *argv[]){
-    
-   
-     
-    
-    
+int main(int argc, char *argv[]){ 
     
     
     
@@ -26,4 +25,10 @@ int main(int argc, char *argv[]){
     
     
     return 0;
+}
+
+
+void sendBleMsg(void){
+
+    
 }
